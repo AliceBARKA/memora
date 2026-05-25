c9d2a3ba53ca6a5867209bd9d5694269b0d7589c
Manal Chabane <manal.chabane@etu.univ-amu.fr>
2026-05-25 10:40:43 +0200

    corrections of a few bugs in ai and quizz gen

--- Backend/courses/admin.py
+++ Backend/courses/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import CoursePDF, Deck, Flashcard
-# Register your models here.
+from .models import CoursePDF, Deck, Flashcard, Quiz, QuizQuestion, QuizAttempt
 
 admin.site.register(CoursePDF)
 admin.site.register(Deck)
 admin.site.register(Flashcard)
+admin.site.register(Quiz)
+admin.site.register(QuizQuestion)
+admin.site.register(QuizAttempt)
