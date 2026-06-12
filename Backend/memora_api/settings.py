d693657c13e08066773901e95a10a517cdd47ce0
HP <alice.barka@etu.univ-amu.fr>
2026-06-12 23:16:50 +0200

    Correction CORS local et GitHub

--- Backend/memora_api/settings.py
+++ Backend/memora_api/settings.py
@@ -201,10 +201,14 @@ if not DEBUG:
 
 CORS_ALLOWED_ORIGINS = [
     "https://alicebarka.github.io",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://alicebarka.github.io",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 STATIC_URL = "/static/"
